965cf1434930d5bc53016240d69047192a4b848a
jjayeon <autumn.chiu.136@gmail.com>
2020-08-26 10:49:28 -0400

    v basic canvas integration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import input from "@jjy/input";
 
-input.bind(" ", function () {
-  const newtext = document.createElement("p");
-  newtext.innerHTML = "haha nice";
-  document.body.appendChild(newtext);
+const app = document.getElementById("app");
+const c = document.createElement("canvas");
+
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+c.id = "canvas";
+
+app.appendChild(c);
+
+console.log(c.width);
+console.log(c.height);
+
+const ctx = c.getContext("2d");
+
+input.bind("r", function () {
+  ctx.fillStyle = "#fdd";
+  ctx.fillRect(0, 0, c.width, c.height);
+});
+
+input.bind("g", function () {
+  ctx.fillStyle = "#dfd";
+  ctx.fillRect(0, 0, c.width, c.height);
+});
+
+input.bind("b", function () {
+  ctx.fillStyle = "#ddf";
+  ctx.fillRect(0, 0, c.width, c.height);
 });
