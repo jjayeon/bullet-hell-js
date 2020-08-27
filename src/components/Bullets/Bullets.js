6b58459846a181d5fb652d3ac1f0e8c2ba5709cf
jjayeon <autumn.chiu.136@gmail.com>
2020-08-27 19:58:59 -0400

    code cleanup and refactoring; bullets now know their index in the bigger array

--- src/components/Bullets/Bullets.js
+++ src/components/Bullets/Bullets.js
@@ -1,25 +1,42 @@
 import bulletimgurl from "./bullet.png";
 
 function Bullets(canvas, player) {
+  // some other stuff we need to keep track of.
   this.canvas = canvas;
   this.player = player;
-  this.children = [];
+
+  // state variables.
+  // an internal cooldown on the player's fire rate.
   this.cd = { cur: 0, max: 500 };
+  // bullet speed.
   this.v = 40;
+  // endpoint for the bullets.
   this.end = canvas.width;
+
+  // the bullets themselves.
+  this.children = [];
 
   const bulletimg = new Image();
   bulletimg.src = bulletimgurl;
   this.img = bulletimg;
 }
 
+function Bullet(i, x, y) {
+  this.i = i;
+  this.x = x;
+  this.y = y;
+}
+
 Bullets.prototype.update = function (input, delta) {
   if (input.pressed[" "]) {
     if (this.cd.cur <= 0) {
-      this.children.unshift({
-        x: this.player.x.p + this.player.w,
-        y: this.player.y.p + this.player.h / 2,
-      });
+      this.children.push(
+        new Bullet(
+          this.children.length,
+          this.player.x.p + this.player.width,
+          this.player.y.p + this.player.height / 2
+        )
+      );
       this.cd.cur = this.cd.max;
     }
   }
@@ -31,7 +48,7 @@ Bullets.prototype.update = function (input, delta) {
   for (var i = 0; i < this.children.length; i++) {
     const child = this.children[i];
     if (child.x > this.end) {
-      this.children.splice(i);
+      this.children.splice(i, 1);
     }
   }
 
@@ -42,15 +59,19 @@ Bullets.prototype.update = function (input, delta) {
 
 Bullets.prototype.draw = function () {
   const ctx = this.canvas.getContext("2d");
+
+  // draw a laser sight.
   ctx.beginPath();
   ctx.strokeStyle = "#f88";
   ctx.lineWidth = 1;
   ctx.moveTo(
-    this.player.x.p + this.player.w,
-    this.player.y.p + this.player.h / 2
+    this.player.x.p + this.player.width,
+    this.player.y.p + this.player.height / 2
   );
-  ctx.lineTo(this.end, this.player.y.p + this.player.h / 2);
+  ctx.lineTo(this.end, this.player.y.p + this.player.height / 2);
   ctx.stroke();
+
+  // render each bullet.
   const img = this.img;
   this.children.forEach(function (item) {
     ctx.drawImage(img, item.x, item.y);
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,13 +1,33 @@
 import playerimgurl from "./player.png";
 
+// constructor function for the player.
+
 function Player(canvas, x, y) {
-  this.canvas = canvas;
-  this.w = 50;
-  this.h = 50;
+  this.canvas = canvas; // needed to know where to draw.
 
-  this.x = { p: x, v: 0, a: 1.8, f: 0.3, m: 8, end: canvas.width - this.w };
-  this.y = { p: y, v: 0, a: 1.8, f: 0.3, m: 8, end: canvas.height - this.h };
+  this.width = 50; // this equals the dimensions of player.png. hard coded for now.
+  this.height = 50;
 
+  // p = position, v = velocity, a = acceleration.
+  // fric = friction, max = max speed, end = farthest point on that axis.
+  this.x = {
+    p: x,
+    v: 0,
+    a: 1.8,
+    fric: 0.3,
+    max: 8,
+    end: canvas.width - this.width,
+  };
+  this.y = {
+    p: y,
+    v: 0,
+    a: 1.8,
+    fric: 0.3,
+    max: 8,
+    end: canvas.height - this.height,
+  };
+
+  // the image to render on screen.
   const playerimg = new Image();
   playerimg.src = playerimgurl;
   this.img = playerimg;
@@ -16,17 +36,22 @@ function Player(canvas, x, y) {
 Player.prototype.update = function (input) {
   // update velocity
   function updatev(d, plus, minus) {
-    if (plus && d.v < d.m) {
+    // d = direction, or axis.
+    // "plus" and "minus" indicate directional keys.
+    // x & plus => d, x & minus => a,
+    // y & plus => s, y & minus => w
+    if (plus && d.v < d.max) {
       d.v += d.a;
-    } else if (minus && -d.v < d.m) {
+    } else if (minus && -d.v < d.max) {
       d.v -= d.a;
+      // if no keys are held, apply friction.
     } else if (d.v > 0) {
-      d.v -= d.f;
+      d.v -= d.fric;
       if (d.v < 0) {
         d.v = 0;
       }
     } else if (d.v < 0) {
-      d.v += d.f;
+      d.v += d.fric;
       if (d.v > 0) {
         d.v = 0;
       }
@@ -35,9 +60,12 @@ Player.prototype.update = function (input) {
   // update position
   function updatep(d) {
     const newd = d.p + d.v;
+    // if in bounds
     if (0 < newd && newd < d.end) {
       d.p = newd;
     } else {
+      // if we're here, we hit the edge, so set v to 0.
+      // without this line, the player sticks to the walls in a weird way.
       d.v = 0;
     }
   }
