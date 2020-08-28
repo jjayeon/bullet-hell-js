5fef3032d3b9de140154ba200fb13acda50f7e79
jjayeon <autumn.chiu.136@gmail.com>
2020-08-28 11:21:42 -0400

    beginning the longer, more arduous journey of adding some unit tests

---
+++ src/components/Entity/Entity.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const assert = require("assert");
+
+describe("testy test", function () {
+  it("should just pass"),
+    function () {
+      assert(true);
+    };
+});
