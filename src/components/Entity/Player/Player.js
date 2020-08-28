74cbb05ff585a085648b1a17194dff10fbd93166
jjayeon <autumn.chiu.136@gmail.com>
2020-08-28 15:20:10 -0400

    incredibly minor cleanup

--- src/components/Entity/Player/Player.js
+++ src/components/Entity/Player/Player.js
@@ -1,8 +1,8 @@
 import Entity from "../Entity.js";
 
 // constructor function for the player.
-function Player(canvas, x, y, playerimg) {
-  Entity.call(this, canvas, x, y, 50, 50, playerimg);
+function Player(canvas, x, y, img) {
+  Entity.call(this, canvas, x, y, 50, 50, img);
 }
 
 Player.prototype = Object.create(Entity.prototype);
