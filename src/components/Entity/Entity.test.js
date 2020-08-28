e981862523d88115f706c55432e4a645aa548db2
jjayeon <autumn.chiu.136@gmail.com>
2020-08-28 15:17:02 -0400

    moved physics unit tests into Entity.test.js, where they belong

--- src/components/Entity/Entity.test.js
+++ src/components/Entity/Entity.test.js
@@ -5,57 +5,112 @@ import Entity from "./Entity.js";
 
 describe("testing Entity.js", function () {
   describe("testing creation and properties", function () {
-    var entity;
-    before(function () {
+    function Defaults(x, width) {
+      return {
+        p: x,
+        v: 0,
+        a: 0,
+        fric: 0.3,
+        max: 8,
+        end: width - 50,
+      };
+    }
+    const defaultx = Defaults(200, 800);
+    const defaulty = Defaults(300, 600);
+    var entity, entity2, entity3;
+    beforeEach(function () {
       entity = new Entity(
-        { width: 50, height: 50, success: true },
-        40,
+        { width: 800, height: 600, success: true },
+        200,
+        300,
         50,
-        60,
-        70,
+        50,
         { success: true }
       );
       // canvas and img can't exist without the DOM, so mocha can't really test for them.
+
+      entity2 = new Entity(
+        { width: 800, height: 600, success: true },
+        300,
+        300,
+        100,
+        100
+      );
+      entity3 = new Entity(
+        { width: 800, height: 600, success: true },
+        225,
+        325,
+        100,
+        100
+      );
     });
+
     it("basic variables", function () {
       assert(entity.canvas.success);
-      assert.equal(entity.x, 40, "x ");
-      assert.equal(entity.y, 50, "y");
-      assert.equal(entity.width, 60, "width");
-      assert.equal(entity.height, 70, "height");
+      assert.equal(entity.x, 200, "x ");
+      assert.equal(entity.y, 300, "y");
+      assert.equal(entity.width, 50, "width");
+      assert.equal(entity.height, 50, "height");
       assert(entity.img.success);
     });
     it("x2 and y2", function () {
-      assert.equal(entity.x2, 40 + 60, "x2");
-      assert.equal(entity.y2, 50 + 70, "y2");
+      assert.equal(entity.x2, 200 + 50, "x2");
+      assert.equal(entity.y2, 300 + 50, "y2");
     });
+    // ----------------------------------------------------------------
+    it("starting values for physics", function () {
+      assert.deepEqual(entity.xvals, defaultx);
+      assert.deepEqual(entity.yvals, defaulty);
+    });
+    it("single update going up", function () {
+      var newvals = Defaults(300, 600);
+      newvals.a = -1.8;
+      newvals.v = -1.8;
+      newvals.p = 300 - 1.8;
+      entity.yvals.a = -1.8;
+      entity.update();
+      assert.deepEqual(entity.xvals, defaultx);
+      assert.deepEqual(entity.yvals, newvals);
+    });
+    it("single update going down", function () {
+      var newvals = Defaults(300, 600);
+      newvals.a = 1.8;
+      newvals.v = 1.8;
+      newvals.p = 300 + 1.8;
+      entity.yvals.a = 1.8;
+      entity.update();
+      assert.deepEqual(entity.xvals, defaultx);
+      assert.deepEqual(entity.yvals, newvals);
+    });
+    it("single update going left", function () {
+      var newvals = Defaults(200, 800);
+      newvals.a = -1.8;
+      newvals.v = -1.8;
+      newvals.p = 200 - 1.8;
+      entity.xvals.a = -1.8;
+      entity.update();
+      assert.deepEqual(entity.xvals, newvals);
+      assert.deepEqual(entity.yvals, defaulty);
+    });
+    it("single update going right", function () {
+      var newvals = Defaults(200, 800);
+      newvals.a = 1.8;
+      newvals.v = 1.8;
+      newvals.p = 200 + 1.8;
+      entity.xvals.a = 1.8;
+      entity.update();
+      assert.deepEqual(entity.xvals, newvals);
+      assert.deepEqual(entity.yvals, defaulty);
+    });
+    // ----------------------------------------------------------------
     it("inside()", function () {
-      entity = new Entity({}, 100, 100, 100, 100, {});
       assert.equal(entity.inside(0, 0), false, "0, 0");
       assert.equal(entity.inside(300, 300), false, "300, 300");
-      assert.equal(entity.inside(150, 150), true, "150, 150");
-      assert.equal(entity.inside(100, 100), true, "100, 100");
-      assert.equal(entity.inside(200, 200), true, "200, 200");
+      assert.equal(entity.inside(225, 325), true, "225, 325");
+      assert.equal(entity.inside(200, 300), true, "200, 300");
+      assert.equal(entity.inside(250, 350), true, "250, 350");
     });
     it("collides()", function () {
-      entity = new Entity({}, 100, 100, 100, 100, {});
-      const entity2 = new Entity(
-        { width: 50, height: 50, success: true },
-        300,
-        300,
-        100,
-        100,
-        {}
-      );
-      const entity3 = new Entity(
-        { width: 50, height: 50, success: true },
-        150,
-        150,
-        100,
-        100,
-        {}
-      );
-
       assert(!entity.collides(entity2), "!entity.collides(entity2)");
       assert(!entity2.collides(entity), "!entity2.collides(entity)");
       assert(entity.collides(entity3), "entity.collides(entity3)");
--- src/components/Entity/Player/Player.test.js
+++ src/components/Entity/Player/Player.test.js
@@ -4,18 +4,6 @@ const assert = require("assert").strict;
 import Player from "./Player.js";
 
 describe("testing Player.js", function () {
-  function Defaults(x, width) {
-    return {
-      p: x,
-      v: 0,
-      a: 0,
-      fric: 0.3,
-      max: 8,
-      end: width - 50,
-    };
-  }
-  const defaultx = Defaults(200, 800);
-  const defaulty = Defaults(300, 600);
   var player;
   beforeEach(function () {
     player = new Player({ width: 800, height: 600 }, 200, 300);
@@ -27,49 +15,5 @@ describe("testing Player.js", function () {
     assert.equal(player.height, 50);
     assert.equal(player.x2, 250);
     assert.equal(player.y2, 350);
-  });
-  it("starting values for physics", function () {
-    assert.deepEqual(player.xvals, defaultx);
-    assert.deepEqual(player.yvals, defaulty);
-  });
-  it("single update going up", function () {
-    var newvals = Defaults(300, 600);
-    newvals.a = -1.8;
-    newvals.v = -1.8;
-    newvals.p = 300 - 1.8;
-    player.yvals.a = -1.8;
-    player.update();
-    assert.deepEqual(player.xvals, defaultx);
-    assert.deepEqual(player.yvals, newvals);
-  });
-  it("single update going down", function () {
-    var newvals = Defaults(300, 600);
-    newvals.a = 1.8;
-    newvals.v = 1.8;
-    newvals.p = 300 + 1.8;
-    player.yvals.a = 1.8;
-    player.update();
-    assert.deepEqual(player.xvals, defaultx);
-    assert.deepEqual(player.yvals, newvals);
-  });
-  it("single update going left", function () {
-    var newvals = Defaults(200, 800);
-    newvals.a = -1.8;
-    newvals.v = -1.8;
-    newvals.p = 200 - 1.8;
-    player.xvals.a = -1.8;
-    player.update();
-    assert.deepEqual(player.xvals, newvals);
-    assert.deepEqual(player.yvals, defaulty);
-  });
-  it("single update going right", function () {
-    var newvals = Defaults(200, 800);
-    newvals.a = 1.8;
-    newvals.v = 1.8;
-    newvals.p = 200 + 1.8;
-    player.xvals.a = 1.8;
-    player.update();
-    assert.deepEqual(player.xvals, newvals);
-    assert.deepEqual(player.yvals, defaulty);
   });
 });
